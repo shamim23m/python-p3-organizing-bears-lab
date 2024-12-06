59500cbb602c564f0329ce0b7f2cbec6389be27f
shamim23m <shamimmohamud13@gmail.com>
2024-12-06 15:26:35 +0300

    initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 18, 'M', 'black', 'Sanguine', 1 );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 20, 'M', 'brown', 'Choleric', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 15, 'M', 'brown', 'Melancholic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'M', 'brown', 'Melancholic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 20, 'M', 'black', 'Choleric', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 19, 'M', 'black', 'Phlegmatic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 17, 'M', 'brown', 'Phlegmatic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 16, 'M', 'black', 'Choleric', 1);
